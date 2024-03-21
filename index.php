ffc3a3d014cbbf16a200c3398764c893bd41258c
Alexito2060 <65459410+Alexito2060@users.noreply.github.com>
2024-03-20 21:06:04 -0300

    codigo actualizado
    - Navbar re programada (arreglar: icono del search, script del navbar) 
    - Comentarios de codigo agregados 
    - Icono de la pagina agregado (favicon) 
    - Intento de una galeria de imagenes
    - Seccion del carrito agregada (restante: implementar la logica)

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="assets/css/master.css">
+        <link rel="stylesheet" href="assets/styles/master.css">
+        <link rel="shortcut icon" href="public/errea-logo.png" type="image/x-icon">
         <title>Inicio - Errea</title>
     </head>
     <body class="dark">
-
-    <?php include "reusables/navbar.html" ?>
-
-    </body>
+        <?php include "reusables/navbar.html" ?>
+        <!-- 
+        <div class="slider-container">
+            <li><img src="public/banners/slider-1.png" alt=""></li>
+            <li><img src="public/banners/slider-2.png" alt=""></li>
+            <li><img src="public/banners/slider-3.png" alt=""></li>
+            <li><img src="public/banners/slider-4.png" alt=""></li>
+            <li><img src="public/banners/slider-5.png" alt=""></li>           
+        </div>
+        -->
+        
+    </body> 
 </html>
