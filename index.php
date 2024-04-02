66d821c09d6f4b1a733eb0bf936fa9e7839dc686
Alexito2060 <johsuahartwig123@gmail.com>
2024-04-01 21:23:03 -0300

    cambios realizados
    - slider dejo de funcionar
    - color del body modificado

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="assets/styles/master.css">
         <link rel="shortcut icon" href="public/errea-logo.png" type="image/x-icon">
+        <link rel="stylesheet" href="assets/styles/master.css">
         <link rel="stylesheet" href="assets/styles/slider.css">
         <title>Inicio - Errea</title>
     </head>
     <body class="dark">
         <?php include "reusables/navbar.html" ?>
-        
+        <!--
         <div class="slider-container">
             <ul>
                 <li><img src="public/banners/slider-1.png" alt=""></li>
@@ -20,9 +20,6 @@
                 <li><img src="public/banners/slider-5.png" alt=""></li>
             </ul>         
         </div>
-        <div class="background-celeste">
-
-</div>
-        
+        -->
     </body> 
 </html>
