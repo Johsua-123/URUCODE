ce996f1b80281a5ac2b5766077b972ec63427d4d
Juan-Cruz <juancruzpirotto0805@gmail.com>
2024-04-08 21:37:54 -0300

    avances en el inicio de sesion y slider
    avances en el inicio de sesion y slider

--- index.php
+++ index.php
@@ -10,18 +10,37 @@
     </head>
     <body class="dark">
         <?php include "reusables/navbar.html" ?>
-        <div class="slider-container">
-            <div class ="slider" id="slider">
-<div class="slider__section"><img src="public/banners/slider-1.png" alt="" class="slider__"></div>
-<div class="slider__section"><img src="public/banners/slider-2.png" alt="" class="slider__"></div>
-<div class="slider__section"><img src="public/banners/slider-3.png" alt="" class="slider__"></div>
-<div class="slider__section"><img src="public/banners/slider-4.png" alt="" class="slider__"></div>
-<div class="slider__section"><img src="public/banners/slider-5.png" alt="" class="slider__"></div>
+
+        <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <img src="slider-6.png" alt="">
+                <div>
+                <div class="slide">
+                    <img src="slider-7.png" alt="">
+                <div>
+                <div class="slide">
+                    <img src="slider-8.png" alt="">
+                <div>
+                <div class="slide">
+                    <img src="slider-9.png" alt="">
+                <div>
+                    
+                <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <img src="slider-6.png" alt="">
+                <div>
+                <div class="slide">
+                    <img src="slider-7.png" alt="">
+                <div>
+                <div class="slide">
+                    <img src="slider-8.png" alt="">
+                <div>
+                <div class="slide">
+                    <img src="slider-9.png" alt="">
+                <div>
             </div>
-            <div class="slider__btn slider__btn--right">&#60;</div>
-            <div class="slider__btn slider__btn--left">&#62;</div>
         </div>
-        <script type="application/javascript" src="slider.js"></script>
-    
     </body> 
 </html>
