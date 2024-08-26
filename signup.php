98295564d51e4dcd322ef5b97e2c8e4ca57ee842
Alexito2060 <johsuahartwig123@gmail.com>
2024-08-26 13:59:08 -0300

    ruta imagen favicon editada

--- signup.php
+++ signup.php
@@ -11,7 +11,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="public/errea-logo.png" type="image/x-icon">
+        <link rel="shortcut icon" href="public/icons/logo.png" type="image/x-icon">
         <link rel="stylesheet" href="assets/styles/module.css">
         <link rel="stylesheet" href="assets/styles/auth.css">
         <script src="assets/scripts/auth/signup.js"></script>
