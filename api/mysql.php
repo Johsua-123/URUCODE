e0f1e5764c9800a2043ea78389fa9b321661ddcf
Alexito2060 <johsuahartwig123@gmail.com>
2024-10-31 15:18:04 -0300

    archivo de conexion restablecido al puerto 3306

--- api/mysql.php
+++ api/mysql.php
@@ -1,7 +1,6 @@
 
 <?php 
     
-    $mysql = mysqli_connect("127.0.0.1:3307", "duenio", "duenio", "urucode") 
-    or die("No se establecio conexion con la base de datos" . mysqli_connect_error());
+    $mysql = mysqli_connect("127.0.0.1", "duenio", "duenio", "urucode") or die("No se establecio conexion con la base de datos" . mysqli_connect_error());
 
 ?>
