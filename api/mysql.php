11029cf269c573e9f5abfb12efdbec9345c6950c
Juan-Cruz <juancruzpirotto0805@gmail.com>
2024-11-04 11:31:26 -0300

    Update mysql.php

--- api/mysql.php
+++ api/mysql.php
@@ -1,7 +1,7 @@
 
 <?php 
     
-    $mysql = new mysqli("localhost", "duenio", "duenio", "urucode");
+    $mysql = new mysqli("127.0.0.1", "duenio", "duenio", "urucode");
 
     if ($mysql->connect_error) die("Error al conectarse a la base de datos, porfavor comprueba la configuracion");
 
