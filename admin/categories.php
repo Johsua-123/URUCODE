aa040fa4276241005790de5c686c3d1437386002
Mateo Severo <nihiltwt@gmail.com>
2024-11-05 08:47:58 -0300

    cositas
    tascosi

--- admin/categories.php
+++ admin/categories.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="assets/styles/navbar.css">
     <link rel="stylesheet" href="assets/styles/sidebar.css">
     <link rel="stylesheet" href="assets/styles/categories.css">
+    <script src="assets/scripts/categories.js"></script>
     <title>Categorías | Errea Admin</title>
 </head>
 <body>
@@ -32,10 +33,7 @@
                 <div class="card">
                     <header>
                         <h2>Categorías</h2>
-                        <button type="button" id="addCategoryBtn">
-                            Agregar Categoría
-                            
-                        </button>
+                        <button type="button" id="addCategoryBtn" onclick="toggleModal()">Agregar Categoría</button>
                     </header>
                     <div class="wrapper">
                         <table>
@@ -58,6 +56,28 @@
             </div>
         </main>
     </div>
-    <script src="admin\assets\scripts\categories.js"></script>
+
+    <div id="productModal" class="modal hidden">
+    <div class="modal-content">
+        <h2>Agregar Nueva Categoría</h2>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div>
+                <label for="nombre">Nombre de la Categoría</label>
+                <input type="text" id="nombre" name="nombre" required>
+            </div>
+            <div>
+                <label for="categoria">Subcategoría</label>
+                <input type="text" id="categoria" name="categoria" required>
+            </div>
+            <div>
+                <label for="icono">Ícono</label>
+                <input type="file" id="icono" name="icono" accept=".jpg, .jpeg, .png" required>
+            </div>
+            <button type="submit">Guardar Producto</button>
+            <button type="button" onclick="toggleModal()">Cerrar</button>
+        </form>
+    </div>
+</div>
+
 </body>
 </html>
