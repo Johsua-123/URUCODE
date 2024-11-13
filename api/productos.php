8eef47b2f3aef41d55cc16b0470f9bd77a15ee54
Alexito2060 <johsuahartwig123@gmail.com>
2024-11-13 20:31:03 -0300

    precio solucionado

--- api/productos.php
+++ api/productos.php
@@ -18,7 +18,7 @@
 
         $nombre = $_POST['nombre'];
         $cantidad = $_POST['cantidad'];
-        $precio_venta = $_POST['precio'];
+        $precio = $_POST['precio'];
         $marca = $_POST['marca'];
         $modelo = $_POST['modelo'];
         $descripcion = $_POST['descripcion'];
@@ -68,11 +68,57 @@
 
     if ($accion == "edicion") {
 
+        $nombre = $_POST['nombre'];
+        $cantidad = $_POST['cantidad'];
+        $precio = $_POST['precio'];
+        $marca = $_POST['marca'];
+        $modelo = $_POST['modelo'];
+        $descripcion = $_POST['descripcion'];
+        $categorias_seleccionadas = $_POST['categorias'] ?? [];
+        $fecha = date("Y-m-d H:i:s");
+
+        $imagen = $_FILES['icono'];
+        $ruta = "../public/images/";
+        $imagen_id = null;
+
+        if (!empty($imagen)) {
+
+            if ($imagen['error'] == UPLOAD_ERR_OK) {
         
-        exit;
+                $archivo = pathinfo($imagen['name']);
+                $extension_img = "." . $archivo["extension"];
+                $nombre_img = $archivo["filename"];
+                
+                $stmt = $mysql->prepare("INSERT INTO imagenes (nombre, extension, fecha_creacion) VALUES (?, ?, ?)");
+                $stmt->bind_param("sss", $nombre_img, $extension_img, $fecha);
+
+                $stmt->execute();
+
+                $imagen_id = $stmt->insert_id;
+
+                move_uploaded_file($imagen["tmp_name"], "$ruta/$nombre_img-$imagen_id$extension_img");
+
+            }
+
+        }
+
+        $stmt = $mysql->prepare("UPDATE productos SET nombre=?, cantidad=?, precio_venta=?, marca=?, modelo=?, imagen_id=?, descripcion=?, fecha_creacion=?, fecha_actualizacion=?");
+        $stmt->bind_param("sidssisss", $nombre, $cantidad, $precio, $marca, $modelo, $imagen_id, $descripcion, $fecha, $fecha);
+        $stmt->execute();
+        $producto_id = $stmt->insert_id;
+
+        foreach ($categorias_seleccionadas as $categoria_id) {
+            $stmt_categoria = $mysql->prepare("INSERT INTO productos_categorias (producto_id, categoria_id, fecha_creacion, fecha_actualizacion) VALUES (?, ?, ?, ?)");
+            $stmt_categoria->bind_param("iiss", $producto_id, $categoria_id, $fecha, $fecha);
+            $stmt_categoria->execute();
+        }
+
+        header("Location: $script");
+
     }
 
     if ($accion == "elimianr") {
+
 
     
         exit;
