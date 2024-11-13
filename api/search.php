973a3b41a2150a0a62a007130ddf8745df79d928
Mateo Severo <nihiltwt@gmail.com>
2024-11-12 22:49:25 -0300

    BUSCADOR ANDANDO AGAINFAFASFSGEW

--- api/search.php
+++ api/search.php
@@ -8,8 +8,18 @@ if ($conexion->connect_error) {
 $resultado = [];
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET["search"])) {
-    $busqueda = $_GET['search'];
-    $buscar = "SELECT * FROM productos WHERE en_venta = 1 AND (nombre LIKE '%$busqueda%' OR marca LIKE '%$busqueda%' OR modelo LIKE '%$busqueda%' OR descripcion LIKE '%$busqueda%')";
+    $busqueda = $conexion->real_escape_string($_GET['search']);
+    $buscar = "
+        SELECT productos.*, CONCAT(imagenes.nombre, imagenes.extension) AS imagen_enlace
+        FROM productos
+        LEFT JOIN imagenes ON productos.imagen_id = imagenes.codigo
+        WHERE productos.en_venta = 1 AND (
+            productos.nombre LIKE '%$busqueda%' OR 
+            productos.marca LIKE '%$busqueda%' OR 
+            productos.modelo LIKE '%$busqueda%' OR 
+            productos.descripcion LIKE '%$busqueda%'
+        )
+    ";
     $resultado = mysqli_query($conexion, $buscar);
 }
 ?>
